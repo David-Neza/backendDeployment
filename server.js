a1264ed65e3398758c91a707fa65b152ae7d36e1
neza <tuyishmirend@gmail.com>
2023-03-01 07:59:24 +0200

    added aDB

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const url = process.env.DATABASE;
 const express = require("express");
 const v1 = require("./router/router");
 const app = express();
@@ -5,6 +8,13 @@ const cors = require("cors");
 
 const port = 4000;
 
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("The Database connected successfully.");
+  });
 app.use(express.json());
 
 app.use("/v1", v1);
