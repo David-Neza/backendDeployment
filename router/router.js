610c269901a6cbf38f719ab332f092c37718978c
neza <tuyishmirend@gmail.com>
2023-03-01 07:42:13 +0200

    traines

--- router/router.js
+++ router/router.js
@@ -6,13 +6,22 @@ let students = [
   { id: 2, name: "student2" },
 ];
 
+let trainees = [
+  { id: 1, name: "Cynthia" },
+  { id: 2, name: "Frank" },
+];
+
 router.get("/students", (req, res) => {
+  res.status(200).send(students);
+});
+
+router.get("/trainees", (req, res) => {
   res.status(200).send(students);
 });
 
 router.post("/students", (req, res) => {
   try {
-    const { id, name } = req.body;    
+    const { id, name } = req.body;
     students.push(req.body[0]);
     return res
       .status(201)
